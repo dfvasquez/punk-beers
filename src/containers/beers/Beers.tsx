19c79827304d058402f173758af0ccced810c509
dfvasquez <dfvasquez@uc.cl>
2023-09-14 17:08:25 +0200

    add basic grids layouts to be filled

--- src/containers/beers/Beers.tsx
+++ src/containers/beers/Beers.tsx
@@ -1,5 +1,25 @@
+import Filter from '../../components/filters/Filter'
 import './Beers.css'
 
 export default function Beers() {
-  return <div>Beers</div>
+  return (
+    <div className='beers-container'>
+      <div>
+        <h2>Beers</h2>
+        <Filter />
+      </div>
+      <div className='grid-container'>
+        <div className='grid-item rect1'></div>
+        <div className='grid-item rect2'></div>
+        <div className='grid-item rect3'></div>
+        <div className='grid-item rect1'></div>
+        <div className='grid-item rect2'></div>
+        <div className='grid-item rect3'></div>
+        <div className='grid-item rect1'></div>
+        <div className='grid-item rect2'></div>
+        <div className='grid-item rect3'></div>
+        
+      </div>
+    </div>
+  )
 }
