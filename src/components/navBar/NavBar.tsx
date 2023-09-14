b5eb3b9c91087560434599e43b646ed3daaf563b
dfvasquez <dfvasquez@uc.cl>
2023-09-14 15:51:13 +0200

    add navbar with home back icon

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import classNames from 'classnames'
 import './NavBar.css'
 
 export default function NavBar() {
-  return <div>NavBar</div>
+  const [isScrolling, setIsScrolling] = useState(false)
+  const [scrollTop, setScrollTop] = useState(0)
+
+  useEffect(() => {
+    const onScroll = () => {
+      const currentPosition = window.scrollY
+      setIsScrolling(currentPosition > scrollTop && currentPosition > 60)
+      setScrollTop(currentPosition)
+    }
+
+    window.addEventListener('scroll', onScroll)
+
+    return () => {
+      window.removeEventListener('scroll', onScroll)
+    }
+  }, [scrollTop])
+
+  return (
+    <div className={classNames('navbar active', { hidden: isScrolling, scrollingBack:  scrollTop > 61})}>
+      <Link to='/' className='not-link'>
+        <h2 className='logo'>Punk Beers</h2>
+      </Link>
+    </div>
+  )
 }
