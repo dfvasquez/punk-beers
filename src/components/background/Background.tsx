ea81786dc07494237056b4741565d29f09fd8100
dfvasquez <dfvasquez@uc.cl>
2023-09-14 12:03:20 +0200

    add first background for all the pages

---
+++ src/components/background/Background.tsx
@@ -0,0 +1,10 @@
+import background from '../../assets/background.svg'
+import './Background.css'
+
+export default function Background() {
+  return (
+    <>
+      <img id='background' src={background} alt='Beers' />
+    </>
+  )
+}
