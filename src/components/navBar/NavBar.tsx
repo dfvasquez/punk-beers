523d062150fecf34ccac29803a8ba4fd93c3bde5
dfvasquez <dfvasquez@uc.cl>
2023-09-20 18:24:05 +0200

    hide searchBar

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,36 +1,13 @@
 import { useEffect, useState } from 'react'
-//import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import classNames from 'classnames'
-//import { getBeersByName } from '../../api/Beers'
-//import { constants } from '../../utils/constants'
-//import SearchInput from '../searchBar/SearchBar'
 import './NavBar.css'
-/* import {
-  setBeers,
-  setLoading,
-  setPage,
-  setTotalPages
-} from '../../store/beersSlice' */
 
 export default function NavBar() {
-  // const dispatch = useDispatch()
-  //const { loadingShort } = constants
+
   const [isScrolling, setIsScrolling] = useState(false)
   const [scrollTop, setScrollTop] = useState(0)
 
-  /*  const handleSearch = (query: string) => {
-    dispatch(setLoading(true))
-    dispatch(setPage(1))
-    getBeersByName(query, 1)
-      .then((data) => {
-        dispatch(setBeers(data))
-        dispatch(setTotalPages(data))
-      })
-      .finally(() => {
-        setTimeout(() => dispatch(setLoading(false)), loadingShort)
-      })
-  } */
 
   useEffect(() => {
     const onScroll = () => {
@@ -58,9 +35,6 @@ export default function NavBar() {
         </Link>
       </div>
 
-     {/*  <div className='pages-container'>
-        <SearchInput onSearch={handleSearch} />
-      </div> */}
     </div>
   )
 }
